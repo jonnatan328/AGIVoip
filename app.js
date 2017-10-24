075dac579de4eaa1fe284ce25e8fef1c393bb670
Jonnatan <jrios328@gmail.com>
2017-10-24 13:04:10 -0500

    Options on AGI

--- app.js
+++ app.js
@@ -12,21 +12,31 @@ function handler(context) {
   // console.log(this.params.hi);
   var conn = Connection.getConnection();
   conn.query('SELECT * from user', function(error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0]);
-  conn.end()
+    if (error) throw error;
+    console.log('The solution is: ', results[0]);
+    // conn.end()
   });
+
+  setTimeout(function () {
+
+  }, 10);
 
   Promise.resolve(context.onEvent('variables'))
     .bind({})
     .then(function(vars) {
       console.log('Texto a voz');
-      return textToSpeech(context, {text: 'Joder!!, yo si hablo feo'});
+      textToSpeech(context, {
+        text: 'Por favor ingrese el código del pedido y finalice con la tecla numeral.'
+      });
+      setTimeout(function() {
+        // textToSpeech(context, {text: 'Es como un españolete maluco. Gas!!'});
 
+        context.getData('test2.wav', 40000)
+        // context.sayNumber(43);
+      }, 30);
     })
     .then((status) => {
       console.log(status);
-      // textToSpeech(context, {text: 'Es como un españolete maluco. Gas!!'});
     })
     .then(function() {
       // return context.end();
